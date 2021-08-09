01d9ebeffe15c46c22fc49bab78c1f74647b52ee
Alexnor11 <advance114@gmail.com>
2021-08-09 14:21:16 +0300

    task 3

--- 03_task.py
+++ 03_task.py
@@ -1,14 +1,23 @@
+import operator
+
 files = ['1.txt', '2.txt', '3.txt']
-my_dict = []
+f_dict = {}
+
 for i in files:
     with open(i, 'r', encoding='utf-8') as file:
-        my_dict.append(f"Имя файла: {i} \n")
-        my_dict.append(f"Количество строк: {len(file.readlines())} \n")
-        file.seek(0)
-        my_dict.append(f"{file.read()} \n\n")
-        s = my_dict.copy()
+        text_read = file.readlines()
+    count = len(text_read)
+    f_dict[i] = count
 
-print(s)
+f_sorted = sorted(f_dict.items(), key=operator.itemgetter(1))
 
 with open('03_task.txt', 'w', encoding='utf-8') as f:
-    f.writelines(my_dict)
+    for i in f_sorted:
+        f.write(f"\n{i[0]} \n")
+        f.write(str(i[1]) + '\n')
+
+        with open(i[0], 'r', encoding='utf-8') as f_read:
+            for line in f_read:
+                # print(line)
+                f.write(line)
+
